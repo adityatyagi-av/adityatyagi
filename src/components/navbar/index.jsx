5f4cc1c9482ec31a3ddcc38ecd508116db75eec4
ADITYA TYAGI <adityatyagiav1@gmail.com>
2023-09-23 21:45:53 +0530

    Added navbar code

---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,44 @@
+"use client"
+import React from "react";
+import { useRouter } from "next/navigation";
+import {Tabs, Tab} from "@nextui-org/react";
+
+export default function Navbar() {
+  const router = useRouter();
+  
+  console.log(router)
+  const handleTabChange =(newTab)=>{
+    
+    router.push(`/${newTab}`);
+  }
+  
+  return (
+    <nav className="flex w-full flex-col mt-8">
+      <Tabs 
+        aria-label="Options"         
+        selectedKey={React.key}
+        onSelectionChange={handleTabChange}
+        className="mx-auto"
+        size="lg"
+        variant="light"
+        color="secondary"
+        radius="full"
+      >
+        <Tab key="" title="Home" >
+          
+        </Tab>
+        <Tab key="about" title="About">
+          
+        </Tab>
+        <Tab key="projects" title="Projects">
+          
+        </Tab>
+        <Tab key="blog" title="Blog">
+          
+        </Tab>
+        
+
+      </Tabs>
+    </nav>  
+  );
+}
